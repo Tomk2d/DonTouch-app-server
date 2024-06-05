79a82839faed367e112b4016b0dad5ada82723a1
Tomk2d <uijinshin01@gmail.com>
2024-06-05 14:07:20 +0900

    fix_struct

---
+++ user-server/src/main/java/donTouch/user_server/user/domain/JpaUserRepositoryTest.java
@@ -0,0 +1,11 @@
+package donTouch.user_server.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface JpaUserRepositoryTest extends JpaRepository{
+
+}
