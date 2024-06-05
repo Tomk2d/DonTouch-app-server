0e2ea74a135e7b897b3c86285c58161b62c9db43
Tomk2d <uijinshin01@gmail.com>
2024-06-05 14:51:45 +0900

    fix build.gradle

--- user-server/src/main/java/donTouch/user_server/user/domain/JpaUserRepositoryTest.java
+++ user-server/src/main/java/donTouch/user_server/user/domain/JpaUserRepositoryTest.java
@@ -1,11 +1,12 @@
 package donTouch.user_server.user.domain;
 
+import donTouch.user_server.user.dto.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 
 @Repository
-public interface JpaUserRepositoryTest extends JpaRepository{
+public interface JpaUserRepositoryTest extends JpaRepository<Users, Long>{
 
 }
