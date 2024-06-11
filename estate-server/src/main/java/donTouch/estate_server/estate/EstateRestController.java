14cd558e5d8f46044fcc38177636b3c18b7aa1cf
Tomk2d <uijinshin01@gmail.com>
2024-06-11 10:17:16 +0900

    db data access

--- estate-server/src/main/java/donTouch/estate_server/estate/EstateRestController.java
+++ estate-server/src/main/java/donTouch/estate_server/estate/EstateRestController.java
@@ -1,6 +1,6 @@
 package donTouch.estate_server.estate;
 
-import donTouch.estate_server.estate.service.DbService;
+import donTouch.estate_server.estate.service.DataAccessService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 public class EstateRestController {
-    private final DbService dbService;
+    private final DataAccessService dataAccessService;
 
     @GetMapping("/saveEstate")
     public String saveEstate() {
-        return dbService.saveEstate();
+        return dataAccessService.saveEstate();
     }
 }
