7429b6850f6bc51b2b0bd9935fbff07f301d61eb
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-06-16 22:14:01 +0900

    refactor: delete useless parameters

--- stock-server/src/main/java/donTouch/stock_server/dividend/service/DividendServiceImpl.java
+++ stock-server/src/main/java/donTouch/stock_server/dividend/service/DividendServiceImpl.java
@@ -5,8 +5,8 @@ import donTouch.stock_server.dividend.dto.DividendDTO;
 import donTouch.stock_server.dividend.dto.DividendForm;
 import donTouch.stock_server.krStock.domain.KrStockJpaRepository;
 import donTouch.stock_server.stock.domain.Stock;
+import donTouch.stock_server.usStock.domain.UsStockJpaRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,9 +17,12 @@ import java.util.List;
 public class DividendServiceImpl implements DividendService {
     private final KrStockDividendFixedJpaRepository krStockDividendFixedJpaRepository;
     private final KrStockDividendExpectedJpaRepository krStockDividendExpectedJpaRepository;
+
     private final UsStockDividendFixedJpaRepository usStockDividendFixedJpaRepository;
     private final UsStockDividendExpectedJpaRepository usStockDividendExpectedJpaRepository;
+
     private final KrStockJpaRepository krStockJpaRepository;
+    private final UsStockJpaRepository usStockJpaRepository;
 
     @Override
     public List<DividendDTO> findCalendar(DividendForm dividendForm) {
@@ -30,27 +33,26 @@ public class DividendServiceImpl implements DividendService {
         List<DividendDTO> dividendDTOList = new ArrayList<>();
 
         List<Dividend> dividendList = new ArrayList<>(krStockDividendFixedJpaRepository.findAllByDividendDateBetweenAndKrStockIdIn(dividendForm.getStartDate(), dividendForm.getEndDate(), holdingKrStockIds));
-        dividendDTOList.addAll(convertToDividendDTOList(dividendList, krStockJpaRepository, true));
+        dividendDTOList.addAll(convertToDividendDTOList(dividendList, true));
 
         dividendList = new ArrayList<>(usStockDividendFixedJpaRepository.findAllByDividendDateBetweenAndUsStockIdIn(dividendForm.getStartDate(), dividendForm.getEndDate(), holdingUsStockIds));
-        dividendDTOList.addAll(convertToDividendDTOList(dividendList, krStockJpaRepository, true));
+        dividendDTOList.addAll(convertToDividendDTOList(dividendList, true));
 
         dividendList = new ArrayList<>(krStockDividendExpectedJpaRepository.findAllByDividendDateBetweenAndKrStockIdIn(dividendForm.getStartDate(), dividendForm.getEndDate(), holdingKrStockIds));
-        dividendDTOList.addAll(convertToDividendDTOList(dividendList, krStockJpaRepository, false));
+        dividendDTOList.addAll(convertToDividendDTOList(dividendList, false));
 
         dividendList = new ArrayList<>(usStockDividendExpectedJpaRepository.findAllByDividendDateBetweenAndUsStockIdIn(dividendForm.getStartDate(), dividendForm.getEndDate(), holdingKrStockIds));
-        dividendDTOList.addAll(convertToDividendDTOList(dividendList, krStockJpaRepository, false));
+        dividendDTOList.addAll(convertToDividendDTOList(dividendList, false));
 
         return dividendDTOList;
     }
 
-    <T extends Stock> List<DividendDTO> convertToDividendDTOList(List<Dividend> dividendList, JpaRepository<T, Integer> stockJpaRepository, boolean isFixed) {
+    <T extends Stock> List<DividendDTO> convertToDividendDTOList(List<Dividend> dividendList, boolean isFixed) {
         List<DividendDTO> dividendDTOList = new ArrayList<>();
 
         for (Dividend dividend : dividendList) {
             dividendDTOList.add(dividend.convertToDividendDTO(isFixed));
         }
-
         return dividendDTOList;
     }
 }
