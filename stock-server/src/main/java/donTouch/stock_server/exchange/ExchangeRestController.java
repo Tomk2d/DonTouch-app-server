bd499e1ad9c6266db30a58619f35042553559d93
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-06-18 10:37:35 +0900

    Revert "test: test commit"
    This reverts commit e8aa4854f18597d9a8a8fb7a241aa84f6d09411d.

--- stock-server/src/main/java/donTouch/stock_server/exchange/ExchangeRestController.java
+++ stock-server/src/main/java/donTouch/stock_server/exchange/ExchangeRestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExchangeRestController {
     @GetMapping("/usd")
     public ApiUtils.ApiResult<ExchangeRate> getUsdExchangeRate() {
-        // test commit
         return ApiUtils.success(ExchangeRate.USD);
     }
 }
