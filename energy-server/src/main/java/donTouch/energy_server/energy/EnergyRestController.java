b7259910bd3f2db1b98f305823318717bd9f8154
parkjineon <truewords0113@naver.com>
2024-06-19 18:03:24 +0900

    feat: add cross origin

--- energy-server/src/main/java/donTouch/energy_server/energy/EnergyRestController.java
+++ energy-server/src/main/java/donTouch/energy_server/energy/EnergyRestController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin(origins = {"http://localhost:5174", "http://localhost:5173"})
 public class EnergyRestController {
     private EnergyFundService energyFundService;
     private EnergyFundDetailService energyFundDetailService;
--- order-server/src/main/java/donTouch/order_server/holding/HoldingRestController.java
+++ order-server/src/main/java/donTouch/order_server/holding/HoldingRestController.java
@@ -14,15 +14,12 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @AllArgsConstructor
 @RestController
+@CrossOrigin(origins = {"http://localhost:5174", "http://localhost:5173"})
 public class HoldingRestController {
     private final HoldingEstateFundService holdingEstateFundService;
     private final HoldingEnergyFundService holdingEnergyFundService;
