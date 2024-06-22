a8d0ad6f647be413b6cfd7152b8fa94c146c8284
parkjineon <truewords0113@naver.com>
2024-06-22 15:13:37 +0900

    feat: update investmentType save mehtod

--- user-server/src/main/java/donTouch/user_server/user/service/UserServiceImpl.java
+++ user-server/src/main/java/donTouch/user_server/user/service/UserServiceImpl.java
@@ -27,22 +27,43 @@ public class UserServiceImpl implements UserService {
 
         Integer score = investmentTypeForm.getTotalScore();
         int result;
+        int safeScore;
+        int growthScore;
+        int dividendScore;
 
         if(score<0){
             throw new NullPointerException("점수는 0 또는 양수로 입력하세요.");
         }else if(score <=  7 ){
             result = 1; // 안정형
+            safeScore = 65;
+            growthScore = 5;
+            dividendScore = 30;
         }else if(score <= 14){
             result = 2; // 안정추구형
+            safeScore = 50;
+            growthScore = 20;
+            dividendScore = 30;
         }else if(score <= 21){
             result = 3; // 위험중립형
+            safeScore = 35;
+            growthScore = 35;
+            dividendScore = 30;
         }else if(score <= 28){
             result = 4; // 적극투자형
+            safeScore = 20;
+            growthScore = 50;
+            dividendScore = 30;
         }else{
             result = 5; //공격투자형
+            safeScore = 5;
+            growthScore = 65;
+            dividendScore = 30;
         }
 
         user.setInvestmentType(result);
+        user.setSafeScore(safeScore);
+        user.setGrowthScore(growthScore);
+        user.setDividendScore(dividendScore);
         jpaUserRepository.save(user);
 
         return result;
