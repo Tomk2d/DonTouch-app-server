63eb85a6aa1950a43d513b7f7f8c936718533874
Tomk2d <150140536+Tomk2d@users.noreply.github.com>
2025-08-04 20:03:01 +0900

    refactoring for calculate bankAccount lock

--- user-server/src/main/java/donTouch/user_server/user/service/BankAccountServiceImpl.java
+++ user-server/src/main/java/donTouch/user_server/user/service/BankAccountServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Transactional
 @Slf4j
 @AllArgsConstructor
 @Service
@@ -37,6 +36,7 @@ public class BankAccountServiceImpl implements BankAccountService {
     }
 
     @Override
+    @Transactional
     public String createBank(BankCreateForm bankCreateForm) {
         BankAccount form = getBankAccount(bankCreateForm);
         if (form == null) {
@@ -50,47 +50,44 @@ public class BankAccountServiceImpl implements BankAccountService {
     }
 
     @Override
+    @Transactional
     public BankAccountDto calculateMoney(BankCalculateForm bankCalculateForm) {
-        BankAccount bank = bankRepository.findByUserId(bankCalculateForm.getUserId())
-            .orElseThrow(()-> new NullPointerException("계좌를 찾을 수 없습니다."));
-        Long price = bankCalculateForm.getPrice();
-        Long cash = bank.getCash();
+        try {
+            BankAccount bank = bankRepository.findByUserIdForUpdate(bankCalculateForm.getUserId())
+                .orElseThrow(()-> new NullPointerException("계좌를 찾을 수 없습니다."));
+            Long price = bankCalculateForm.getPrice();
+            Long cash = bank.getCash();
 
-
-        if (price<0 && cash < Math.abs(price)){
-            return null;
-        }else{
-            bank.setCash(cash + price);
-            BankAccount result = bankRepository.save(bank);
-            return bankMapper.toDto(result);
+            if (price<0 && cash < Math.abs(price)){
+                return null;
+            }else{
+                bank.setCash(cash + price);
+                BankAccount result = bankRepository.save(bank);
+                return bankMapper.toDto(result);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("서비스가 혼잡합니다. 잠시 후 다시 시도해주세요.");
         }
     }
 
     @Override
+    @Transactional
     public BankAccountDto calculateAccountMoney(BankCalculateForm bankCalculateForm) {
-        BankAccount bank = bankRepository.findByUserId(bankCalculateForm.getUserId())
+        try {
+            BankAccount bank = bankRepository.findByUserIdForUpdate(bankCalculateForm.getUserId())
                 .orElseThrow(()-> new NullPointerException("계좌를 찾을 수 없습니다."));
-        Long price = bankCalculateForm.getPrice();
-        Long cash = bank.getCash();
+            Long price = bankCalculateForm.getPrice();
+            Long cash = bank.getCash();
 
-
-        Long userId = bankCalculateForm.getUserId();
-        Long inputCash = bankCalculateForm.getPrice();
-
-
-        if(inputCash >= 0){
-            kafkaProducerService.requestAddBankLog(new BankAccountLogDto(userId, inputCash, 1, "입금", LocalDateTime.now()));
-        }else{
-            kafkaProducerService.requestAddBankLog(new BankAccountLogDto(userId, -1*inputCash, 0, "출금", LocalDateTime.now()));
-        }
-
-
-        if (price<0 && cash < Math.abs(price)){
-            return null;
-        }else{
-            bank.setCash(cash + price);
-            BankAccount result = bankRepository.save(bank);
-            return bankMapper.toDto(result);
+            if (price<0 && cash < Math.abs(price)){
+                return null;
+            }else{
+                bank.setCash(cash + price);
+                BankAccount result = bankRepository.save(bank);
+                return bankMapper.toDto(result);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("서비스가 혼잡합니다. 잠시 후 다시 시도해주세요.");
         }
     }
 
