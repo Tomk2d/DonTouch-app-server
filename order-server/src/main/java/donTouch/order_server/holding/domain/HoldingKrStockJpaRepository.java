7226098f21e9589cfd9c3ede9bd9e69261f4a3dd
Tomk2d <150140536+Tomk2d@users.noreply.github.com>
2025-08-07 15:46:22 +0900

    Update HoldingKrStockJpaRepository.java

--- order-server/src/main/java/donTouch/order_server/holding/domain/HoldingKrStockJpaRepository.java
+++ order-server/src/main/java/donTouch/order_server/holding/domain/HoldingKrStockJpaRepository.java
@@ -1,14 +1,21 @@
 package donTouch.order_server.holding.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface HoldingKrStockJpaRepository extends JpaRepository<HoldingKrStock, Long> {
-    Optional<HoldingKrStock> findByUserIdAndKrStockId(Long userId, String krStockId);
-
-    List<HoldingKrStock> findAllByUserId(Long userId);
+    public Optional<HoldingKrStock> findByUserIdAndKrStockId(Long userId, String krStockId);
+    public List<HoldingKrStock> findAllByUserId(Long userId);
+    
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT h FROM HoldingKrStock h WHERE h.user.id = :userId AND h.krStockId = :krStockId")
+    public Optional<HoldingKrStock> findByUserIdAndKrStockIdWithLock(@Param("userId") Long userId, @Param("krStockId") String krStockId);
 }
