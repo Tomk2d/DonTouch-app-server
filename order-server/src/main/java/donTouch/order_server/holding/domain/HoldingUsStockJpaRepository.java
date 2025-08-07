31310760e3f7ee5186303a064a014e668e96553a
Tomk2d <150140536+Tomk2d@users.noreply.github.com>
2025-08-07 15:46:03 +0900

    refactoring for update holding US stock lock - repository

--- order-server/src/main/java/donTouch/order_server/holding/domain/HoldingUsStockJpaRepository.java
+++ order-server/src/main/java/donTouch/order_server/holding/domain/HoldingUsStockJpaRepository.java
@@ -1,14 +1,21 @@
 package donTouch.order_server.holding.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface HoldingUsStockJpaRepository extends JpaRepository<HoldingUsStock, Long> {
-    Optional<HoldingUsStock> findByUserIdAndUsStockId(Long userId, String krStockId);
-
-    List<HoldingUsStock> findAllByUserId(Long userId);
+    public Optional<HoldingUsStock> findByUserIdAndUsStockId(Long userId, String usStockId);
+    public List<HoldingUsStock> findAllByUserId(Long userId);
+    
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT h FROM HoldingUsStock h WHERE h.user.id = :userId AND h.usStockId = :usStockId")
+    public Optional<HoldingUsStock> findByUserIdAndUsStockIdWithLock(@Param("userId") Long userId, @Param("usStockId") String usStockId);
 }
